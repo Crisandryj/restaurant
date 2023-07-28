80b500ee0004160ac5b5c9ec99ee9b7b60b7152b
crisandryj <crisandryj@gmail.com>
2023-07-28 10:27:59 -0400

    Rename

--- src/home.js
+++ src/home.js
@@ -2,13 +2,16 @@
 function appendHome(){
   //clear html
   document.querySelector('#content').innerHTML = ""
+  
   // create container
   const container = document.querySelector('#content')
+
   //add heading and image to container
   const image = document.createElement('img')
   const heading = document.createElement('h1')
-  heading.textContent = "Restaurant"
+  heading.textContent = "Le Bernardin"
   container.appendChild(heading)
+
   //select image from files and src
   image.src = "/rest.jpeg"
   container.appendChild(image)
